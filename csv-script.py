09267c54b2bbc699394a4d0a7abd0e6ffb758ba0
Mohammed.Saeb <Mohammed.saeb98@hotmail.com>
2020-05-15 15:08:15 +0300

    added table and images

--- csv-script.py
+++ csv-script.py
@@ -1,17 +1,23 @@
 # importing the needed libraries
-import base64, csv, os
+import base64, csv, os, xlrd
 
-file = open('file.csv')
-csv_file = csv.reader(file)
+# file = open('table.xlsx')
+# csv_file = csv.reader(file)
 
-# reading the directory list of files
-entries = os.listdir('/home/omer/Desktop/images/')
+# # reading the directory list of files
+# entries = os.listdir('./images/')
 
-# loop into images and insert it to the correspondent name in the csv file
-for entry in entries:
-    for i in csv_file:
-        if csv_file[i][0] == entry:
-            # converting images to binary and save it into the correspondent column
-            csv_file[i][5] = base64.b64encode(entry.read())
+# # loop into images and insert it to the correspondent name in the csv file
+# for entry in entries:
+#     for i in csv_file:
+#         if csv_file[i][4] == entry:
+#             # converting images to binary and save it into the correspondent column
+#             csv_file[i][5] = base64.b64encode(entry.read())
 
 # save the new csv
+workbook = xlrd.open_workbook("table.xlsx")
+sheet = workbook.sheet_by_index(0)
+
+for rowx in range(sheet.nrows):
+    values = sheet.row_values(rowx)
+    print(values)
