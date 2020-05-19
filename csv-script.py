90893b1bf946ea5560289f2f5d6cdb8e673b5e95
Mohammed.Saeb <Mohammed.saeb98@hotmail.com>
2020-05-19 09:42:57 +0300

    first commit

--- csv-script.py
+++ csv-script.py
@@ -1,42 +1,39 @@
 # importing the needed libraries
-<<<<<<< HEAD
-import base64, csv_creator, os, numpy as np
+import base64, csv, os
 
-file = open('/home/omer/Desktop/images/1.csv')
-csv_array = np.genfromtxt(file, delimiter=";", skip_header=1)
-    #csv.reader(file)
+# a function reading images in base64 format
+def encode_image(path):
+    with open("./images/{}".format(path), "rb") as img:
+        return base64.b64encode(img.read())
 
-# reading the directory list of files
-entries = os.listdir('/home/omer/Desktop/images/images')
+#opening the old CSV
+with open('table.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
 
-# loop into images and insert it to the correspondent name in the csv file
-for entry in entries:
-    mod_entry = os.path.splitext(entry)[0]
-    for i in csv_array:
-        if csv_array[i][0] == mod_entry:
-            # converting images to binary and save it into the correspondent column
-            csv_array[i][5] = base64.b64encode(entry.read())
-=======
-import base64, csv, os, xlrd
+    #creating a new CSV
+    with open('new_table.csv', 'w') as new_file:
 
-# file = open('table.xlsx')
-# csv_file = csv.reader(file)
+        #creating the header for the new CSV
+        fieldnames = ['name',"Country of Origin","Unit of Quantity",
+            "Product description", "product_code", "images(base64)"]
 
-# # reading the directory list of files
-# entries = os.listdir('./images/')
+        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames)
+        csv_writer.writeheader() #writing the head of the CSV file
 
-# # loop into images and insert it to the correspondent name in the csv file
-# for entry in entries:
-#     for i in csv_file:
-#         if csv_file[i][4] == entry:
-#             # converting images to binary and save it into the correspondent column
-#             csv_file[i][5] = base64.b64encode(entry.read())
->>>>>>> 09267c54b2bbc699394a4d0a7abd0e6ffb758ba0
+        images = os.listdir('./images')
 
-# save the new csv
-workbook = xlrd.open_workbook("table.xlsx")
-sheet = workbook.sheet_by_index(0)
+        for old_line in csv_reader:
 
-for rowx in range(sheet.nrows):
-    values = sheet.row_values(rowx)
-    print(values)
+            new_img = {"images(base64)" : ""}
+            new_line = dict(old_line, **new_img)
+
+            for img in images:
+                # checking if the product code is equal to the image name
+                # without the '.png' format characters
+                if old_line['product_code'] == img[:-4]: 
+                    new_img = {"images(base64)" : encode_image(img)}
+                    # for combining two dictionaries into one
+                    new_line = dict(old_line, **new_img)
+                    break
+
+            csv_writer.writerow(new_line)
